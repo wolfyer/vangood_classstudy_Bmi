51c11438457a8e255088e646046c5fd3d6bad98b
Class <a90023y@gmail.com>
2022-03-15 13:52:53 +0800

    menu

--- app/src/main/java/com/vangood/bmi/MainActivity.kt
+++ app/src/main/java/com/vangood/bmi/MainActivity.kt
@@ -6,6 +6,7 @@ import android.util.Log
 import android.view.View
 import androidx.activity.viewModels
 import androidx.appcompat.app.AlertDialog
+import androidx.fragment.app.Fragment
 import com.vangood.bmi.databinding.ActivityMainBinding
 
 
@@ -14,17 +15,19 @@ class MainActivity : AppCompatActivity() {
         private val TAG = MainActivity::class.java.simpleName
     }
     private lateinit var binding :ActivityMainBinding
-
+    //many fragment
+    val fragments = mutableListOf<Fragment>()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
+//onCreate not yet enter view
         initFragment()
     }
 
     private fun initFragment() {
-        val guess1to10Fragment = BlankFragment()
+        fragments.add(0,BlankFragment())
+        //val guess1to10Fragment = BlankFragment()
         /*val transaction =supportFragmentManager.beginTransaction()
             transaction.add(R.id.container,guess1to10Fragment)
             .commit()*/
@@ -32,7 +35,7 @@ class MainActivity : AppCompatActivity() {
         //.run run run bye bye return = Int can not use
         //.apply return = Fragment bag.apply put tea coffee than to person
         supportFragmentManager.beginTransaction().apply {
-            add(R.id.container,guess1to10Fragment)
+            add(R.id.container,fragments[0])
             commit()
         }
     }
